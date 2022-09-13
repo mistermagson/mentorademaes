d474c765c3fa875ab161f5aa92fa9f2da4001428
Magson Magalhaes <magson@gmail.com>
2022-09-12 22:20:46 -0400

    Esqueleto da aplicação

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+function HomePage() {
+    return (
+        <>
+            <figure>foto </figure>
+            <div>nome</div>
+            <ul>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+            </ul>
+        </>
+        )
+}
+
+export default HomePage
